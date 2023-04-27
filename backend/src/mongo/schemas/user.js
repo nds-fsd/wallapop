ab509bc3c492e6953947e939c7fa2a4f63783643
JMBACCA <juanmartinbacca@gmail.com>
2023-04-27 21:27:26 +0200

    Correcion de backend

--- backend/src/mongo/schemas/user.js
+++ backend/src/mongo/schemas/user.js
@@ -1,7 +1,4 @@
-const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const secret = process.env.JWT_SECRET;
+const { Schema } = require("mongoose");
 
 const userSchema = new Schema({
   name: { type: String, required: true },
@@ -15,47 +12,4 @@ const userSchema = new Schema({
   gender: { type: String },
 });
 
-userSchema.pre("save", function (next) {
-  const user = this;
-
-  //si no se ha cambiado la contraseña, seguimos
-  if (!user.isModified("password")) return next();
-
-  //brcypt es una libreria que genera "hashes", encriptamos la contraseña
-  bcrypt.genSalt(10, function (err, salt) {
-    if (err) return next(err);
-
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-
-      // si no ha habido error en el encryptado, guardamos
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compareSync(password, this.password);
-};
-
-userSchema.methods.generateJWT = function () {
-  const today = new Date();
-  const expirationDate = new Date();
-
-  expirationDate.setDate(today.getDate() + 60);
-
-  let payload = {
-    id: this._id,
-    name: this.name,
-    email: this.email,
-  };
-
-  return jwt.sign(payload, secret, {
-    expiresIn: parseInt(expirationDate.getTime() / 1000, 10),
-  });
-};
-
-const User = model("user", userSchema);
-
-module.exports = User;
+module.exports = userSchema;
