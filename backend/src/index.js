8f91e19159ee5235c1311bcefa81cc1d10825f1b
Mar Badia <mar.badia@tiendeo.com>
2023-05-27 10:26:14 +0200

    cambiando puerto

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,7 @@
-
-const express = require('express');
-const {connectDB} =  require("./mongo/connection");
-const cors = require('cors');
-const productRouter = require('./mongo/routers/productRouter')
+const express = require("express");
+const { connectDB } = require("./mongo/connection");
+const cors = require("cors");
+const productRouter = require("./mongo/routers/productRouter");
 const userRouter = require("./mongo/routers/userRouter");
 const categoryRouter = require("./mongo/routers/categoryRouter");
 
@@ -11,12 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
-app.use('/products', productRouter);
+app.use("/products", productRouter);
 
 app.use("/category", categoryRouter);
 
-connectDB().then(() => console.log("Connected to database!"))
+connectDB().then(() => console.log("Connected to database!"));
 
-const server = app.listen(3001, () => {
-  console.log("Server is up and running ⚡");
+const PORT = process.env.PORT || 3001;
+const server = app.listen(PORT, () => {
+  console.log(`Server is up and running ⚡ ${PORT}`);
 });
