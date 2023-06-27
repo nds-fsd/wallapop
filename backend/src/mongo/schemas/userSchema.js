bcc5fe32f12759ef309d8aeb287af7b8c635ad3c
Luciano Churio <124820115+LuchoChurio@users.noreply.github.com>
2023-06-27 11:44:34 +0200

    Update userSchema.js

--- backend/src/mongo/schemas/userSchema.js
+++ backend/src/mongo/schemas/userSchema.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
-  photo: { type: String },
+  photo: { type: String, default: "https://res.cloudinary.com/dvogntdp2/image/upload/v1687858972/Imagen_de_WhatsApp_2023-06-27_a_las_11.41.34_bwm87e.jpg" },
   adress: { type: String },
   birthday: { type: Date },
   gender: { type: String },
