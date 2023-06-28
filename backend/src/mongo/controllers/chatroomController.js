2c5bedf116aae98ec40e1d95a38ad66f12d5d941
Mar Badia <mar.badia@tiendeo.com>
2023-06-28 20:36:59 +0200

    plantilla crud

--- backend/src/mongo/controllers/chatroomController.js
+++ backend/src/mongo/controllers/chatroomController.js
@@ -1,65 +1,15 @@
 const { getAll, deleteOne, getID } = require("../../services/crud-service");
 const chatroomModel = require("../models/chatroomModel");
 
-// const getChatRoomByID = async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const chatroomById = await chatroomModel.findById(id).exec();
-//     res.status(200).json(chatroomById);
-//   } catch (error) {
-//     res.status(404).json({ error: "Sorry, can't find this chat room" });
-//   }
-// };
-
 const getChatRoomByID = getID({
   model: chatroomModel,
   populationFields: [],
 });
 
-// const getProductByChatRoom = async (req, res) => {
-//   const { chatroom } = req.params;
-//   try {
-//     const chat = await chatroomModel
-//       .findById(chatroom)
-//       .populate("product_id")
-//       .populate("buyer_id")
-//       .populate("owner_id")
-//       .exec();
-
-//     res.status(200).json(chat);
-//   } catch (error) {
-//     return console.log(error);
-//   }
-// };
-
 const getProductByChatRoom = getID({
   model: chatroomModel,
   populationFields: ["owner_id", "product_id", "buyer_id"],
 });
-
-// const getAllChats = async (req, res) => {
-//   const userId = req.jwtPayload.id;
-//   try {
-//     const chatrooms = await chatroomModel
-//       .find({
-//         $or: [
-//           {
-//             owner_id: userId,
-//           },
-//           {
-//             buyer_id: userId,
-//           },
-//         ],
-//       })
-//       .populate("owner_id")
-//       .populate("product_id")
-//       .populate("buyer_id")
-//       .exec();
-//     res.status(200).json(chatrooms);
-//   } catch (error) {
-//     res.status(404).json({ error: "Sorry, can't find this chat room" });
-//   }
-// };
 
 // ME FALTA EL OR
 const getAllChats = getAll({
@@ -102,6 +52,64 @@ const deleteChatRoomById = deleteOne({
   model: chatroomModel,
 });
 
+module.exports = {
+  getAllChats,
+  deleteChatRoomById,
+  postChatRoom,
+  getChatRoomByID,
+  getProductByChatRoom,
+};
+
+// const getChatRoomByID = async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const chatroomById = await chatroomModel.findById(id).exec();
+//     res.status(200).json(chatroomById);
+//   } catch (error) {
+//     res.status(404).json({ error: "Sorry, can't find this chat room" });
+//   }
+// };
+
+// const getProductByChatRoom = async (req, res) => {
+//   const { chatroom } = req.params;
+//   try {
+//     const chat = await chatroomModel
+//       .findById(chatroom)
+//       .populate("product_id")
+//       .populate("buyer_id")
+//       .populate("owner_id")
+//       .exec();
+
+//     res.status(200).json(chat);
+//   } catch (error) {
+//     return console.log(error);
+//   }
+// };
+
+// const getAllChats = async (req, res) => {
+//   const userId = req.jwtPayload.id;
+//   try {
+//     const chatrooms = await chatroomModel
+//       .find({
+//         $or: [
+//           {
+//             owner_id: userId,
+//           },
+//           {
+//             buyer_id: userId,
+//           },
+//         ],
+//       })
+//       .populate("owner_id")
+//       .populate("product_id")
+//       .populate("buyer_id")
+//       .exec();
+//     res.status(200).json(chatrooms);
+//   } catch (error) {
+//     res.status(404).json({ error: "Sorry, can't find this chat room" });
+//   }
+// };
+
 // const deleteChatRoomById = async (req, res) => {
 //   const { id } = req.params;
 //   try {
@@ -121,11 +129,3 @@ const deleteChatRoomById = deleteOne({
 //       .json({ error: "An error occurred while deleting the chat room" });
 //   }
 // };
-
-module.exports = {
-  getAllChats,
-  deleteChatRoomById,
-  postChatRoom,
-  getChatRoomByID,
-  getProductByChatRoom,
-};
